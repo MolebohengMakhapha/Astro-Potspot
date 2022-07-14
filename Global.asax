baebfdd39302b54ea47c611abad06be013b6118e
Lebo <starskyrenay@gmail.com>
2022-07-14 14:55:15 +0200

    added source file and solution file

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_219014430.MvcApplication" Language="C#" %>
---
+++ Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace _219014430.Models
+{
+    public class Contact
+    {
+        [Key]
+        public string ContactId { get; set; }
+
+        [Display(Name = "Full Name and Surname")]
+        [Required(ErrorMessage = "Fullname is Required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Your name may not be less than 2 characters")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^(?!\.)(""([^""\r\\]|\\[""\r\\])*""|" + @"([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)" + @"@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$")]
+        [StringLength(70)]
+        [Display(Name = "Your Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Message is required")]
+        [StringLength(100, MinimumLength = 2)]
+        [Display(Name = "Your Message")]
+        public string Message { get; set; }
+    }
+}
